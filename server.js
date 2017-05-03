73ec1fbe8d4beb3db827e7599b2423c4a8e22347
Adam Youngblood <ayoungbl0d@gmail.com>
2017-05-02 19:56:29 -0500

    minor changes

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 var express         = require('express'),
     mongoose        = require('mongoose'),
     port            = process.env.PORT || 3000,
+    methodOverride  = require('method-override'),
+    bodyParser      = require('body-parser'),
+    hbs             = require('hbs'),
+    passport        = require('passport'),
+    LocalStrategy   = require('passport-local').Strategy,
+
     app             = express();
+
+    mongoose.Promise = global.Promise;
+
+var serviceId       = 'GncUbyqPyvc';
+
+
+var User            = require('./models/user.js'),
+    Character       = require('./models/character.js'),
